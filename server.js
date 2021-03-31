14c97aae4779be391669a50b4b3a55969a1413fe
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-03-31 19:55:30 -0300

    Deployment

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "build")));
+}
+app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
